58a4e0e91fc80bc6b79d165da69ac45e9f033c4d
Tobias Bielohlawek <tobi@soundcloud.com>
2013-04-30 21:35:02 +0200

    cleanup, remove unnecessary stuff

--- project.clj
+++ project.clj
@@ -1,31 +1,22 @@
 (defproject msc-generator-service "1.0.0-SNAPSHOT"
-  :description "Cool new project to do things and stuff"
+  :description "Renerate crazy graphs fast!"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "1.1.3"]
-                 [cheshire "5.0.1"]
 
-                 [ring "1.1.6"]
-                 [ring/ring-jetty-adapter "1.1.0"]
-                 [ring/ring-devel "1.1.0"]
-
-                 [org.clojure/tools.logging "0.2.3"]
-                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jdmk/jmxtools
-                                                    com.sun.jmx/jmxri]]
-                 [org.slf4j/slf4j-log4j12 "1.6.6"]
-
-                 ;memcached        [clojurewerkz/spyglass "1.1.0-beta1"]
-                 ;circuit breaker  [circuit-breaker "0.1.6-SNAPSHOT"]
-                 ;http             [http.async.client "0.5.2"]
-
-                 [org.clojars.soundcloud/exceptional-clj "0.7.7"]
-                 [com.soundcloud/statsyj "1.0.0"]]
+                 [ring/ring-core "1.1.1"]
+                 [ring-server "0.2.5"]
+                 [compojure "1.1.1"]
+                 [ring-json-response "0.2.0"]
+                 [net.sourceforge.plantuml/plantuml "7947"]
+                 [markdown-clj "0.9.8"]
+                 [clj-time "0.4.4"]
+                 [tentacles "0.2.4"]
+                 [enlive "1.0.1"]
+                 [environ "0.2.1"]
+                 [ring-mock "0.1.3"]]
 
   :min-lein-version "2.0.0"
-
-  :plugins [[lein-ring "0.8.3"][lein-exec "0.2.1"]]
 
   :main msc-generator-service.start
 
@@ -35,9 +26,10 @@
               :plugins      [[lein-midje "2.0.4"]
                              [lein-kibit "0.0.8"]]}}
 
-  :ring {:handler msc-generator-service.web/app}
+  ; :ring {:handler msc-generator-service.web/app}
 
-  :repositories [["SoundCloud Internal - Snapshots"       "http://maven.int.s-cloud.net/content/repositories/snapshots/"]
-                 ["SoundCloud Internal - Releases"        "http://maven.int.s-cloud.net/content/repositories/releases/"]
-                 ["SoundCloud Internal - Proxy Snapshots" "http://maven.int.s-cloud.net/content/groups/proxy_snapshots/"]
-                 ["SoundCloud Internal - Proxy Releases"  "http://maven.int.s-cloud.net/content/groups/proxy_releases/"]])
+  ; :repositories [["SoundCloud Internal - Snapshots"       "http://maven.int.s-cloud.net/content/repositories/snapshots/"]
+  ;                ["SoundCloud Internal - Releases"        "http://maven.int.s-cloud.net/content/repositories/releases/"]
+  ;                ["SoundCloud Internal - Proxy Snapshots" "http://maven.int.s-cloud.net/content/groups/proxy_snapshots/"]
+  ;                ["SoundCloud Internal - Proxy Releases"  "http://maven.int.s-cloud.net/content/groups/proxy_releases/"]]
+  )
