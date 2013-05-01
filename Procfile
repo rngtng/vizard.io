b7368f141661cb4ce9002dee0c75c87a7b35fdfa
Tobias Bielohlawek <tobi@soundcloud.com>
2013-05-01 11:43:47 +0200

    initial maven/bundler/bazooka setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: vendor/jruby-1.7.1/bin/jruby -S bundle exec vendor/jruby-1.7.1/bin/jruby server.rb
---
+++ server.rb
@@ -0,0 +1,8 @@
+require "rubygems"
+require "bundler/setup"
+
+require "sinatra"
+
+get '/' do
+    "Hello JRuby World!"
+end
